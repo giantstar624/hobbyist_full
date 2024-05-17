4d8c9378338eebd6a11f3963054d6c389c48167c
giantstar624 <brightcn624@gmail.com>
2024-05-16 19:48:58 -0700

    update

--- Backend/src/api/database/index.ts
+++ Backend/src/api/database/index.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import { logger } from '../config/logger';
 
-dotenv.config({ path: __dirname + '/.env' });
+dotenv.config({ path: __dirname + '/../../.env' });
 
 
 const connectDatabase = async () => {
@@ -10,8 +10,6 @@ const connectDatabase = async () => {
     let uri = process.env.DB_CONNECTION
     const testDB = process.env.TEST_DB || 'no';
     if (testDB == 'yes' && process.env.TEST_DB_CONNECTION) uri = process.env.TEST_DB_CONNECTION
-    console.log(__dirname+'/.env')
-    console.log(uri)
     mongoose.connect(uri, {
       //   useNewUrlParser: true,
       //   useUnifiedTopology: true,
